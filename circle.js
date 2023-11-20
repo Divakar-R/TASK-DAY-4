22e52e2bd81faa2697763fa703120a20ef480ee7
Divakar-R <148629037+Divakar-R@users.noreply.github.com>
2023-11-20 20:59:05 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,3 +1,5 @@
+//get circumference of circle
+
 class Circle{
     constructor(radius,color){
         this.radius=1.0;
@@ -8,4 +10,49 @@ class Circle{
     }
 }
 const mycircle=new Circle();
-console.log(mycircle.getcircumference);
+console.log(mycircle.getcircumference());
+
+
+//get Area of circle
+
+class Circle{
+    constructor(radius,color){
+        this.radius=radius;
+        this.color=color;
+    }
+getarea(){
+    return Math.PI*this.radius*this.radius;
+}
+}
+const mycircle=new Circle(1.0,"red");
+console.log(mycircle.getarea());
+
+
+//get radius
+
+class Circle{
+    constructor(radius,color){
+        this.radius=radius;
+        this.color=color;
+    }
+    getradius(){
+        return this.radius;
+    }
+}
+const mycircle=new Circle(1.0,"red");
+console.log(mycircle.getradius());
+
+
+//get color
+
+class Circle{
+    constructor(radius,color){
+        this.radius=radius;
+        this.color=color;
+    }
+    getcolor(){
+        return this.color;
+    }
+}
+const mycircle=new Circle(1.0,"red");
+console.log(mycircle.getcolor());
